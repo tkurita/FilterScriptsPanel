4b096dabef877ad9ca8f3b7945136a64cfd2c6fd
Tetsuro Kurita <tkurita@mac.com>
2005-08-22 17:40:27 +0000

    userdefaults の設定部を applicationWillFnishLaunching に移動

--- AppController.m
+++ AppController.m
@@ -1,5 +1,7 @@
 #import "AppController.h"
 
+#define useLog 0
+
 @implementation AppController
 
 - (void)checkQuit:(NSTimer *)aTimer
@@ -24,26 +26,35 @@
 
 - (void)anApplicationIsTerminated:(NSNotification *)aNotification
 {
-	//NSLog(@"anApplicationIsTerminated");
+#if useLog
+	NSLog(@"anApplicationIsTerminated");
+#endif
 	NSString *appName = [[aNotification userInfo] objectForKey:@"NSApplicationName"];
-	//NSLog(appName);
 	if ([appName isEqualToString:@"mi"] ) [[NSApplication sharedApplication] terminate:self];
 }
 
 #pragma mark delegate of NSApplication
+- (void)applicationWillFinishLaunching:(NSNotification *)aNotification
+{
+#if useLog
+	NSLog(@"start applicationWillFinishLaunching");
+#endif
+	NSString *defaultsPlistPath = [[NSBundle mainBundle] pathForResource:@"UserDefaults" ofType:@"plist"];
+	NSDictionary *defautlsDict = [NSDictionary dictionaryWithContentsOfFile:defaultsPlistPath];
+	NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+	[userDefaults registerDefaults:defautlsDict];	
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-	//NSLog(@"start applicationDidFinishLaunching");
+#if useLog
+	NSLog(@"start applicationDidFinishLaunching");
+#endif
 	appQuitTimer = [NSTimer scheduledTimerWithTimeInterval:60*60 target:self selector:@selector(checkQuit:) userInfo:nil repeats:YES];
 	[appQuitTimer retain];
 	
 	NSNotificationCenter *notifyCenter = [[NSWorkspace sharedWorkspace] notificationCenter];
 	[notifyCenter addObserver:self selector:@selector(anApplicationIsTerminated:) name:NSWorkspaceDidTerminateApplicationNotification object:nil];
-	
-	NSString *defaultsPlistPath = [[NSBundle mainBundle] pathForResource:@"UserDefaults" ofType:@"plist"];
-	NSDictionary *defautlsDict = [NSDictionary dictionaryWithContentsOfFile:defaultsPlistPath];
-	NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-	[userDefaults registerDefaults:defautlsDict];
 }
 
 @end
